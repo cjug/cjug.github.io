d3cf44fff09139bf3d7b773c7a4f463c834d271b
travis <travis@testing-docker-b2fbcdb7-5ec6-4e5f-a5f3-063d90802541>
2017-03-24 03:49:22 +0000

    Publish of Github pages from Gradle.

--- js/cjug.js
+++ js/cjug.js
@@ -5,8 +5,7 @@ $(document).ready(function(){
         success: function(dataWeGotViaJsonp){
         	var source   = $("#meetup-template").html();
         	var template = Handlebars.compile(source);
-        	console.log(dataWeGotViaJsonp);
-        	$('#meetup-main').append(template(dataWeGotViaJsonp['data'][0]));
+        	$('#meetup-main').append(template(dataWeGotViaJsonp));
         }
 	})
 });
