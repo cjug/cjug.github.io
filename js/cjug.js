b8ca378c89c3550da7f3ecc43b1f0d9cf1121441
travis <travis@testing-docker-5a66ac87-2050-4137-9ec9-91bb4719b401>
2017-03-24 03:18:31 +0000

    Publish of Github pages from Gradle.

--- js/cjug.js
+++ js/cjug.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$.ajax({
-        url: 'https://api.meetup.com/ChicagoJug/events?photo-host=public&page=20&sig_id=13119970&sig=a0149059896b0b89fcaae31aeebe1be10df14570',
+        url: 'https://api.meetup.com/ChicagoJug/events?photo-host=public&page=20&sig_id=13119970&sig=a0149059896b0b89fcaae31aeebe1be10df14570&callback=jsonCallback',
         dataType: 'jsonp'
     });
 });
