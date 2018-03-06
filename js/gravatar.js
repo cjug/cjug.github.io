b9af8b4317c5e8d764e007444783e91b81d0384a
Travis CI User <travis@example.org>
2018-03-06 04:56:19 +0000

    Publish of Github pages from Gradle.

--- js/gravatar.js
+++ js/gravatar.js
@@ -1,6 +1,5 @@
 function gravatar( email, size ) {
     var trimmed = email.toLowerCase().trim();
     var hash = md5(trimmed);
-    console.log("https://www.gravatar.com/avatar/" + hash + "?size="+size + "&dt=" + new Date().getTime());
     return "https://www.gravatar.com/avatar/" + hash + "?size="+size + "&dt=" + new Date().getTime();
 }
