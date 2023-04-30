18ca502cdbdf9cd76b36a0f24bd4a650b88fa50f
cjug <83935880+joshaustintech@users.noreply.github.com>
2023-04-30 05:05:00 +0000

    Update from https://github.com/cjug/cjug.org/commit/84b89e779a8b3c853848469f7d13580d069141df

--- js/gravatar.js
+++
@@ -1,5 +0,0 @@
-function gravatar( email, size ) {
-    var trimmed = email.toLowerCase().trim();
-    var hash = md5(trimmed);
-    return "https://www.gravatar.com/avatar/" + hash + "?size="+size + "&dt=" + new Date().getTime();
-}
